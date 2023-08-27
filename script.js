b8efe0d68aa15da79b57539d9fc95811d1ff3f04
deekshuk12 <98563491+deekshuk12@users.noreply.github.com>
2023-08-27 02:00:21 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 
+function navigateToPage(pageNumber) {
+    const pages = document.querySelectorAll('.page');
+    pages.forEach((page, index) => {
+        if (index === pageNumber - 1) {
+            page.style.transform = 'translateX(0)';
+        } else {
+            page.style.transform = 'translateX(100%)';
+        }
+    });
+}
+
